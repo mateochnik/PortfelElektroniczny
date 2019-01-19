a7f3a8b0d11f55eb2948b37831f205f51685af29
Mateusz Ochnik <mateusz.ochnik@gmail.com>
2019-01-19 13:47:49 +0100

    commit11

--- PortfelElektroniczny/app/forms.py
+++ PortfelElektroniczny/app/forms.py
@@ -21,7 +21,7 @@ class BootstrapAuthenticationForm(AuthenticationForm):
     
 
 class Rejestracja(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    first_name = forms.CharField(max_length=32, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
