f999d781e7210f4f01477485b489754985e822a6
Mateusz Ochnik <mateusz.ochnik@gmail.com>
2019-01-23 15:19:40 +0100

    commit

--- PortfelElektroniczny/app/models.py
+++ PortfelElektroniczny/app/models.py
@@ -5,22 +5,52 @@ Definition of models.
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-
+from datetime import date
 
 CURRENCY_STORE_FIELD = getattr(settings,
         'WALLET_CURRENCY_STORE_FIELD', models.BigIntegerField)
 
+class DochodCategory(models.Model):
+    description = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return self.description
+
+class Dochod(models.Model):
+    
+    dochod_id = models.BigIntegerField(default = 0)
+    description = models.CharField(max_length=1000)
+    amount = models.DecimalField(max_digits=18, decimal_places=2, default=0)
+    date = models.DateField(default=timezone.now)
+    category = models.ForeignKey(DochodCategory)
+    
+    def __str__(self):
+        return self.description
+    def getAmount():
+        return amount
+
+    class Meta:
+        ordering = ['-description', '-pk']
+    
 
 class Wallet(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    listaDochodow=[]
+    listaWydatkow=[]
 
+    dochod = models.ManyToManyField(Dochod)
+    
+
+    
     current_balance = CURRENCY_STORE_FIELD(default=0)
 
     created_at = models.DateTimeField(auto_now_add = True)
 
-    def deposit(self,value):
+    def dodajDochod(self,dochod):
         
+        listaDochodow.append()
+        value = dochod.getAmount()
         self.transaction_set.create(
             value = value,
             running_balance = self.current_balance + value)
@@ -28,7 +58,7 @@ class Wallet(models.Model):
         self.save()
 
 
-    def withdraw(self, value):
+    def dodajWydatek(self, value):
 
         self.transaction_set.create(
             value=-value,
